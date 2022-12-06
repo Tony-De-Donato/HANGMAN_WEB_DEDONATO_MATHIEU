9b93f9a9b695c744afcc5b4e0582448ae1a3b405
Tony-De-Donato <tonydedo13@gmail.com>
2022-12-06 09:36:03 +0100

    ajust

--- main.go
+++ main.go
@@ -5,21 +5,25 @@ import (
 	"net/http"
 )
 
-type User struct {
-	Choix   string
-	Success bool
+type Page struct {
+	mot        string
+	tentatives int
+	pose       string
+	deja       string
 }
 
 func main() {
-	tmpl1 := template.Must(template.ParseFiles("acceuil_hangman.html"))
+	tmpl1 := template.Must(template.ParseFiles("jeu_hangman.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			tmpl1.Execute(w, nil)
 			return
 		}
-		details := User{
-			Choix:   r.FormValue("choix"),
-			Success: true,
+		details := Page{
+			mot:        "LEMOT",
+			tentatives: 10,
+			pose:       "img",
+			deja:       "a,g,s,t,motdefou,rgb",
 		}
 		tmpl1.Execute(w, details)
 	})
