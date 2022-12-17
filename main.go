0a5f42a1213c6333bd8f93ee34068204ff83cbae
Tony-De-Donato <tonydedo13@gmail.com>
2022-12-17 11:22:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,19 +6,26 @@ import (
 )
 
 func main() {
-	tmpl1 := template.Must(template.ParseFiles("acceuil_hangman.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		if r.FormValue("Hangman1") == "1" {
-			tmpl1 = template.Must(template.ParseFiles("jeu_hangman.html"))
-			lancer(1)
-		}
-		tmpl1.Execute(w, nil)
-	})
-
+	http.HandleFunc("/", acceuil)
+	http.HandleFunc("/jeu", jeu_hangman)
 	http.ListenAndServe(":80", nil)
 
 }
-func lancer(nbr int) {
+func acceuil(w http.ResponseWriter, r *http.Request) {
+	tmpl1, err := template.ParseFiles("acceuil_hangman.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	tmpl1.Execute(w, nil)
+	if r.FormValue("Hangman1") == "1" {
+		jeu_hangman(w, r)
+	}
+}
 
+func jeu_hangman(w http.ResponseWriter, r *http.Request) {
+	tmpl2, err := template.ParseFiles("jeu_hangman.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	tmpl2.Execute(w, nil)
 }
