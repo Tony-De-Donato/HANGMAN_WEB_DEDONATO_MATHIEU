b721bf66390052c8c7ed3b35ecad0d455d956e5f
Tony-De-Donato <tonydedo13@gmail.com>
2022-12-18 15:44:35 +0100

    réorganisation

--- Hangman/main.go
+++ Hangman/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", acceuil)
 	http.HandleFunc("/jeu", jeu_hangman)
-	http.ListenAndServe(":80", nil)
+	log.Fatal(http.ListenAndServe(":80", nil))
 
 }
 func acceuil(w http.ResponseWriter, r *http.Request) {
 	tmpl1, err := template.ParseFiles("acceuil_hangman.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	tmpl1.Execute(w, nil)
 	if r.FormValue("Hangman1") == "1" {
@@ -23,9 +25,11 @@ func acceuil(w http.ResponseWriter, r *http.Request) {
 }
 
 func jeu_hangman(w http.ResponseWriter, r *http.Request) {
+
 	tmpl2, err := template.ParseFiles("jeu_hangman.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	tmpl2.Execute(w, nil)
 }
